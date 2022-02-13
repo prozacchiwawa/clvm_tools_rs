c6314d3cc32d744baad430007dc7b36fa5c6d70a
arty <art.yerkes@gmail.com>
2022-02-13 14:42:49 -0800

    first successful runs starting

--- src/new/bins/fuzz.rs
+++ src/new/bins/fuzz.rs
@@ -7,34 +7,45 @@ use clvm_tools_rs::classic::clvm_tools::stages::stage_0::DefaultProgramRunner;
 use clvm_tools_rs::compiler::prims;
 use clvm_tools_rs::compiler::srcloc::Srcloc;
 use clvm_tools_rs::compiler::clvm::run;
+use clvm_tools_rs::compiler::compiler::{ DefaultCompilerOpts, compile_file };
 
 use clvm_tools_rs::compiler::sexp::SExp;
 use clvm_tools_rs::compiler::fuzzer::FuzzProgram;
+use clvm_tools_rs::compiler::runtypes::RunFailure;
+
 use rand::random;
 use std::env;
 
 fn main() {
-    let loc = Srcloc::start(&"*prog*".to_string());
     let mut allocator = Allocator::new();
+    let opts = Rc::new(DefaultCompilerOpts::new(&"*prog*".to_string()));
     let runner = Rc::new(DefaultProgramRunner::new());
     let prim_map = prims::prim_map();
 
     // Sickos: hahaha YES
     let prog: FuzzProgram = random();
-    let args: SExp = prog.random_args();
 
     println!("prog: {}", prog.to_sexp().to_string());
 
-    let args = prog.random_args();
-    println!("args: {}", args.to_string());
-
-    run(
+    compile_file(
         &mut allocator,
         runner.clone(),
-        prim_map,
-        Rc::new(prog.to_sexp()),
-        Rc::new(args)
-    ).map(|after_run| {
+        opts.clone(),
+        &prog.to_sexp().to_string()
+    ).map_err(|e| RunFailure::RunErr(e.0, e.1)).and_then(|compiled| {
+        println!("compiled: {}", compiled.to_string());
+
+        let args = prog.random_args();
+        println!("args: {}", args.to_string());
+
+        run(
+            &mut allocator,
+            runner.clone(),
+            prim_map,
+            Rc::new(compiled),
+            Rc::new(args)
+        )
+    }).map(|after_run| {
         println!("result: {}", after_run.to_string());
     }).unwrap_or_else(|e| panic!("error: {:?}", e))
 }
