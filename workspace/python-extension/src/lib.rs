14c112446fe53d8bb9a9c3e5aeb1cf141555741c
arty <art.yerkes@gmail.com>
2022-04-28 12:25:20 -0700

    Fix configuration

--- workspace/python-extension/src/lib.rs
+++ workspace/python-extension/src/lib.rs
@@ -2,11 +2,9 @@
 extern crate lazy_static;
 
 #[macro_use]
-#[cfg(all(not(test), not(target_family = "wasm"), feature = "extension-module"))]
 extern crate pyo3;
 
 extern crate tempfile;
 
 // Python impl
-#[cfg(all(not(test), not(target_family = "wasm"), feature = "extension-module"))]
 mod py;
