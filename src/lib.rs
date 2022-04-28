c5c729bf772c241a347e171d885215c35cdc2181
arty <art.yerkes@gmail.com>
2022-04-28 11:43:40 -0700

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ extern crate indoc;
 extern crate do_notation;
 
 #[macro_use]
-#[cfg(all(not(test), not(target_family = "wasm"), feature="extension-module"))]
+#[cfg(all(not(test), not(target_family = "wasm"), feature = "extension-module"))]
 extern crate pyo3;
 
 extern crate tempfile;
@@ -20,10 +20,6 @@ mod util;
 
 pub mod classic;
 pub mod compiler;
-
-// Python impl
-#[cfg(all(not(test), not(target_family = "wasm"), feature="extension-module"))]
-mod py;
 
 #[cfg(test)]
 mod tests;
--- workspace/python-extension/src/lib.rs
+++ workspace/python-extension/src/lib.rs
@@ -2,11 +2,11 @@
 extern crate lazy_static;
 
 #[macro_use]
-#[cfg(all(not(test), not(target_family = "wasm"), feature="extension-module"))]
+#[cfg(all(not(test), not(target_family = "wasm"), feature = "extension-module"))]
 extern crate pyo3;
 
 extern crate tempfile;
 
 // Python impl
-#[cfg(all(not(test), not(target_family = "wasm"), feature="extension-module"))]
+#[cfg(all(not(test), not(target_family = "wasm"), feature = "extension-module"))]
 mod py;
