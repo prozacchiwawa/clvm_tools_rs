4bfe3d7583538df97ea2f592845dff720857be78
arty <art.yerkes@gmail.com>
2022-04-28 14:22:06 -0700

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ extern crate indoc;
 extern crate do_notation;
 
 #[macro_use]
-#[cfg(all(not(test), not(target_family = "wasm"), feature="extension-module"))]
+#[cfg(all(not(test), not(target_family = "wasm"), feature = "extension-module"))]
 extern crate pyo3;
 
 extern crate tempfile;
@@ -22,7 +22,7 @@ pub mod classic;
 pub mod compiler;
 
 // Python impl
-#[cfg(all(not(test), not(target_family = "wasm"), feature="extension-module"))]
+#[cfg(all(not(test), not(target_family = "wasm"), feature = "extension-module"))]
 mod py;
 
 #[cfg(test)]
