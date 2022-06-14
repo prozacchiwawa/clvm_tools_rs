01c78c97c5e84c9a37ed189a537ab38844dfb6df
arty <art.yerkes@gmail.com>
2022-06-13 17:28:46 -0700

    Fix in chrome

--- repl/index.js
+++ repl/index.js
@@ -9,63 +9,65 @@ function repl() {
 	console.log('about to require clvm_tools_rs');
 	var r = require('./clvm_tools_rs.js');
 
-	var repl = r.create_repl();
-	console.log('repl',repl);
+  r.then((r) => {
+	    var repl = r.create_repl();
+	    console.log('repl',repl);
 
-	var replInput = document.getElementById('repl-input');
-  var cm = CodeMirror.fromTextArea(replInput, {
-        lineNumbers: false,
-        mode: 'scheme'
-  });
-	var replButton = document.getElementById('repl-send-input');
-
-  function resetInput(s) {
 	    var replInput = document.getElementById('repl-input');
-      if (replInput) {
-          replInput.value = s;
-      }
-      if (cm) {
-          cm.setValue(s);
-      }
-  }
+      var cm = CodeMirror.fromTextArea(replInput, {
+          lineNumbers: false,
+          mode: 'scheme'
+      });
+	    var replButton = document.getElementById('repl-send-input');
 
-  function addTranscript(classes, s, assign) {
-	    var replTranscript = document.getElementById('repl-transcript');
-	    var lineElement = document.createElement('pre');
-      lineElement.setAttribute('class', classes);
-	    lineElement.appendChild(document.createTextNode(s));
-      if (assign) {
-          var buttonElement = document.createElement('button');
-          buttonElement.setAttribute('class', 'text-copy-button');
-          buttonElement.addEventListener('click', () => {
-              navigator.clipboard.writeText(s);
-          });
-          lineElement.appendChild(buttonElement);
-          buttonElement = document.createElement('button');
-          buttonElement.setAttribute('class', 'text-repeat-button');
-          buttonElement.addEventListener('click', () => {
-              resetInput(s);
-          });
-          lineElement.appendChild(buttonElement);
+      function resetInput(s) {
+	        var replInput = document.getElementById('repl-input');
+          if (replInput) {
+              replInput.value = s;
+          }
+          if (cm) {
+              cm.setValue(s);
+          }
       }
-	    replTranscript.appendChild(lineElement);
-	}
 
-	replButton.addEventListener('click', () => {
-	    var replInput = document.getElementById('repl-input');
-	    var input = cm.getValue().trim();
-	    var result = r.repl_run_string(repl, input);
-
-	    addTranscript('repl-t-input', input, true);
-      if (result === null || result === undefined) {
-          addTranscript('repl-t-waiting', '...');
-	    } else if (result.error !== undefined) {
-	        addTranscript('repl-t-error', 'E ' + result.error);
-	    } else if (result) {
-	        addTranscript('repl-t-output', '> ' + r.sexp_to_string(result));
+      function addTranscript(classes, s, assign) {
+	        var replTranscript = document.getElementById('repl-transcript');
+	        var lineElement = document.createElement('pre');
+          lineElement.setAttribute('class', classes);
+	        lineElement.appendChild(document.createTextNode(s));
+          if (assign) {
+              var buttonElement = document.createElement('button');
+              buttonElement.setAttribute('class', 'text-copy-button');
+              buttonElement.addEventListener('click', () => {
+                  navigator.clipboard.writeText(s);
+              });
+              lineElement.appendChild(buttonElement);
+              buttonElement = document.createElement('button');
+              buttonElement.setAttribute('class', 'text-repeat-button');
+              buttonElement.addEventListener('click', () => {
+                  resetInput(s);
+              });
+              lineElement.appendChild(buttonElement);
+          }
+	        replTranscript.appendChild(lineElement);
 	    }
-      scrollToBottom('repl-transcript-holder');
-	});
+
+	    replButton.addEventListener('click', () => {
+	        var replInput = document.getElementById('repl-input');
+	        var input = cm.getValue().trim();
+	        var result = r.repl_run_string(repl, input);
+
+	        addTranscript('repl-t-input', input, true);
+          if (result === null || result === undefined) {
+              addTranscript('repl-t-waiting', '...');
+	        } else if (result.error !== undefined) {
+	            addTranscript('repl-t-error', 'E ' + result.error);
+	        } else if (result) {
+	            addTranscript('repl-t-output', '> ' + r.sexp_to_string(result));
+	        }
+          scrollToBottom('repl-transcript-holder');
+	    });
+  });
 }
 
 axios.get('clvm_tools_rs_bg.wasm', {responseType: 'blob'}).then((resp) => {
--- repl/webpack.config.js
+++ repl/webpack.config.js
@@ -40,4 +40,5 @@ module.exports = {
 	}
     },
     devtool: 'source-map',
+    experiments: {asyncWebAssembly: true}
 };
